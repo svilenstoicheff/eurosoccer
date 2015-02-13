1e8c9738adbaca299eaea605f52df5f6395a6ecd
Svilen Stoicheff <svilen.stoicheff@networkedinsights.com>
2015-02-13 16:37:48 -0600

    updates

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,29 +1,57 @@
 angular.module('Soccer', []);
 
 angular.module('Soccer')
-  .controller('Teams', function($scope){
-  	console.log('inside Teams');
+  .controller('Teams', function($scope, $http){
+  	//console.log('inside Teams');
+    //$scope.teamName = "test";
 
-  	$scope.getTeamsData = function(){
-  		alert('here');
-  	};
-console.log($scope);
-
-  //		$http.get(url).
-  //success(function(data, status, headers, config) {
+  	$scope.getTeamsData = function(url){
+  	//alert(url);
+    $http.defaults.headers.get = { 'X-Auth-Token' : '55e2b001494e4a19b5ea2aa10ada3c7e' }
+    $http.get(url).
+    success(function(data, status, headers, config) {
     // this callback will be called asynchronously
     // when the response is available
-  //  console.log(data);
-  //}).
-  //error(function(data, status, headers, config) {
+      console.log(data);
+      $scope.teamName = data.name;
+      $scope.teamMarketValue = data.squadMarketValue;
+      $scope.teamInfo = true;
+      var playerList = document.querySelector('ul.playerList');
+      $(playerList).html('');
+      //console.log(playerList);
+      
+
+      //jQuery for now
+          $('a.seePlayers').on('click', function(){
+            $.ajax({
+              url: data._links.players.href, 
+              headers: {"X-Auth-Token": "55e2b001494e4a19b5ea2aa10ada3c7e"}, 
+              dataType: 'json'
+            }).done(function(teams_data){
+              $('ul.playerList').html('');
+              $.each(teams_data.players, function(i, player){
+                $('ul.playerList').append('<li>'+ player.name +'</li>');
+              });
+              
+            });
+          });
+
+
+    }).
+    error(function(data, status, headers, config) {
     // called asynchronously if an error occurs
     // or server returns response with an error status.
-  //});
+    });
+      console.log($scope.teamName);
+  	};
+//console.log($scope);
 
+ 
   });
 
   $(document).ready(function(){
-  	$('.teamBadge, .teamName').on('click', function(e){
+  /*
+  	$('.teamName').on('click', function(e){
   		var url = $(this).attr('href');
   		e.preventDefault();
   		$('ul.playerList').html('');
@@ -32,7 +60,6 @@ console.log($scope);
   				headers:{"X-Auth-Token": "55e2b001494e4a19b5ea2aa10ada3c7e"}, 
   				dataType: 'json'
   			}).done(function(data){
-  				console.log(data);
   				$('section.teamInfo h2').html(data.name);
   				$('section.teamInfo .marketValue').html(data.squadMarketValue);
   				$('section.teamInfo').show();
@@ -43,16 +70,15 @@ console.log($scope);
   						headers: {"X-Auth-Token": "55e2b001494e4a19b5ea2aa10ada3c7e"}, 
   						dataType: 'json'
   					}).done(function(teams_data){
-  						console.log(teams_data);
+              $('ul.playerList').html('');
   						$.each(teams_data.players, function(i, player){
   							$('ul.playerList').append('<li>'+ player.name +'</li>');
   						});
   						
   					});
   				});
-
   			});
-
-  		//alert(url);
   	});
+
+    */
   });
