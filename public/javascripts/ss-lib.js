c1e389589d30707d448829a31383b05cfa19fc9b
Svilen Stoicheff <svilen.stoicheff@networkedinsights.com>
2015-02-18 14:46:31 -0600

    adding new files

---
+++ public/javascripts/ss-lib.js
@@ -0,0 +1,27 @@
+//JS lib functions
+var SS = SS || {};
+SS.formatDate = function (datestr){
+        if (datestr === null || datestr === '' || datestr === undefined){
+          return '';
+        }
+          var formattedDateStr = datestr.replace(/-/g, '/'),
+              rawDate = new Date(formattedDateStr), 
+              oldDate = rawDate.toLocaleDateString().split('/'),
+              newDate = '';
+
+              for(var i = 0; i < oldDate.length; i++){
+                if (oldDate[i].length === 1) {
+                  oldDate[i] = '0' + oldDate[i];
+                }
+                newDate += oldDate[i] + '/';
+              }
+          var lastChar = newDate.length - 1;
+          if (newDate.charAt(lastChar) === '/'){
+            newDate = newDate.substr(0, lastChar);
+          }
+        return newDate;
+      }
+SS.euroRate;
+SS.convertEurUsd = function(){
+
+}
